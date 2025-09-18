fbb4281c0bd173d6f91eb8c84f2464c43623f2dd
Robert Eric Reeves <robertericreeves@gmail.com>
2025-09-18 10:21:12 -0500

    Fix Sphinx configuration warnings
    - Set language = 'en' instead of None to fix language warning
    - Remove deprecated html_theme_path and get_html_theme_path() usage
    - Remove unused sphinx_rtd_theme import
    
    This eliminates the 2 warnings to achieve a clean documentation build.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -2,8 +2,6 @@
 # Sphinx Configuration for Titan documentation
 #
 
-import sphinx_rtd_theme
-
 # -- Project information -----------------------------------------------------
 
 project = u'titan'
@@ -25,12 +23,11 @@ extensions = ['recommonmark', 'sphinx.ext.ifconfig']
 templates_path = ['_templates']
 source_suffix = ['.rst', '.md']
 exclude_patterns = ['Thumbs.db', '.DS_Store']
-language = None
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_style = 'css/titan.css'
 html_static_path = ['_static']
 html_show_sourcelink = False
