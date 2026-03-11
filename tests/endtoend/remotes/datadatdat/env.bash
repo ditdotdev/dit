a694c7a726f0e8c28f77b667f3e150a23a575076
Robert Eric Reeves <robertericreeves@gmail.com>
2026-03-11 16:09:07 -0500

    Update DOWNLOAD_TEST_VERSION from v1.7.0 to v1.7.1

--- tests/endtoend/remotes/datadatdat/env.bash
+++ tests/endtoend/remotes/datadatdat/env.bash
@@ -76,7 +76,7 @@ else
   export APIKEY_TEST_ORG="apikeytest"
 
   # Download API test version
-  export DOWNLOAD_TEST_VERSION="${DOWNLOAD_TEST_VERSION:-v1.7.0}"
+  export DOWNLOAD_TEST_VERSION="${DOWNLOAD_TEST_VERSION:-v1.7.1}"
 
   # Helper: execute SQL against local postgres container
   run_sql() {
